164d0d1a855298b0868397902642b1c6c86a796a
xaviSalazar <xavicoel@gmail.com>
2022-01-27 15:26:56 +0100

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <div className='grid grid-cols-12 gap-6 px-5 lg:px-48 my-14'>
+    <div  className='col-span-12 bg-white lg:col-span-3 rounded-2x1 p-4 text-center'>
+      Sidebar
+      </div>
+    <div  className='col-span-12 bg-white lg:col-span-9 rounded-2x1'>
+        <Component {...pageProps} />
+    </div>
+
+  </div>
+
+  )
+
+  
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+const index = () => {
+    return (
+        <div>
+            <h1>Bye bye world</h1>
+        </div>
+    )
+}
+
+export default index
