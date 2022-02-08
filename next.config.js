752959d457fed04d11b748d1a15add2c78f64ffb
xaviSalazar <xavicoel@gmail.com>
2022-02-08 14:40:26 +0100

    file: workflows1.0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     loader: 'custom',
     domains: ["media-exp1.licdn.com"],
