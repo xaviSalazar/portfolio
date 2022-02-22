a7a651241beb7b3581a93ca692ff45225991f217
xaviSalazar <xavicoel@gmail.com>
2022-02-22 10:31:04 +0100

    feat: heroku websocket

--- components/Bot.tsx
+++ components/Bot.tsx
@@ -5,9 +5,10 @@ import  { ChatLayout }  from "./chatLayout";
 var ws = null;
 var messages = []
 
-messages.unshift( { name : "bot", message: 'Hi welcome to my Beta version Bot, I am currently testing some AI models and also searching the best hosting site so you can communicate with my chatbot'} )
+messages.unshift( { name : "bot", message: 'Hi welcome to my Beta version Bot, I am currently testing some NLP knowledge, but I need data!!!'} )
 messages.unshift( { name : "bot", message: 'To let you know I am a C++ Developer'} )
 messages.unshift( { name : "bot", message: 'I am currently working in a mission at VEDECOM where I develop C++ drivers in a Linux Environment'} )
+messages.unshift( { name : "bot", message: 'What would you like to know?'} )
 
 
 
@@ -16,7 +17,7 @@ export default function Bot() {
 
   const [Message, setMessage] = useState('');
 
-  if (ws==null) ws = new WebSocket ('ws://localhost:8765');
+  if (ws==null) ws = new WebSocket ('ws://stormy-mountain-52583.herokuapp.com/');
 
      ws.onopen = () => {
     console.log("connected")
