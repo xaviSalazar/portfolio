2f7e89af9389e1d0f2aed6050ec783960d566c27
xaviSalazar <xavicoel@gmail.com>
2022-02-23 09:24:36 +0100

    fix: linter in chat layout

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -2,11 +2,11 @@ import { NextApiResponse,NextApiRequest } from "next";
 import {services} from '../../data'
 
 
-export default (req:NextApiRequest, res:NextApiResponse) => {
+// export default (req:NextApiRequest, res:NextApiResponse) => {
 
-    // by default get request
-    console.log(services)
-    res.status(200).json({services})
+//     // by default get request
+//     console.log(services)
+//     res.status(200).json({services})
 
 
-}
+// }
