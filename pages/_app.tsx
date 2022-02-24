09919ebdadf82834eb4cff6e72325af9dfb8a8c3
xaviSalazar <xavicoel@gmail.com>
2022-02-24 16:13:59 +0100

    style: button removed to chat layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,30 +4,15 @@ import '../styles/globals.css'
 import {ThemeProvider} from 'next-themes'
 import { AnimatePresence } from "framer-motion"
 import { useState, useEffect } from 'react'
-import { RiWechat2Line } from 'react-icons/ri'
-import { AiOutlineCloseCircle } from 'react-icons/ai'
+
 import Bot  from '../components/Bot'
 
 
 function MyApp({ Component, pageProps, router }) {
-
-  const [chatButton, setChatButton] = useState(true);
-
   
   return (
   <ThemeProvider attribute='class'>
   <div>
-
- 
-      < button 
-        onClick={() => {setChatButton(!chatButton)}}
-      >
-        {
-          chatButton ? 
-        <RiWechat2Line size={50} className=" animate-bounce fixed p-1 rounded-full bottom-0 right-0 focus:outline-none bg-rose-500 dark:bg-dark-200"/> : <AiOutlineCloseCircle size={30} className="fixed p-1 z-30 rounded-full bottom-0 right-0 focus:outline-none bg-gray-200 dark:bg-dark-200" />
-        }
-      </button>
-
 
   <div className='grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32' >
     <div  className='col-span-12 bg-white dark:bg-dark-500 lg:col-span-3 rounded-2x1 p-4 text-center shadow-custom-light dark:shadow-custom-dark'>
@@ -39,13 +24,13 @@ function MyApp({ Component, pageProps, router }) {
           <Component {...pageProps} key = {router.route} />
         </AnimatePresence>
     </div>
+    <div>
+    <Bot/>
+    </div>
   </div>
-  {
-      chatButton ? (
-        <div> </div>
-      ) : <Bot/>
 
-  }
+  
+  
   </div>
   </ThemeProvider>
   )
