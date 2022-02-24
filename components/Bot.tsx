b4ec2a098447124bb602b103cb8fba1860739675
xaviSalazar <xavicoel@gmail.com>
2022-02-24 16:12:58 +0100

    fix: open and close websocket

--- components/Bot.tsx
+++ components/Bot.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Component, useEffect, useState } from "react";
 import  { ChatLayout }  from "./chatLayout";
+import { RiWechat2Line } from 'react-icons/ri'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 var ws = null;
 var messages = []
@@ -10,50 +12,73 @@ messages.unshift( { name : "bot", message: 'To let you know I am a C++ Developer
 messages.unshift( { name : "bot", message: 'I am currently working in a mission at VEDECOM where I develop C++ drivers in a Linux Environment'} )
 messages.unshift( { name : "bot", message: 'What would you like to know?'} )
 
-
 export default function Bot() {
 
   const [Message, setMessage] = useState('');
-  const [connectStatus, setConnectStatus] = useState(true);
+  const [chatButton, setChatButton] = useState(false);
+  const [connectStatus, setConnectStatus] = useState(false);
  
-  if (ws==null) 
-  {
-    setConnectStatus(false);
+  const openConnection = () => {
+    
+    setChatButton(true)
     ws = new WebSocket ('wss://stormy-mountain-52583.herokuapp.com/');
-  } 
 
     ws.onopen = () => {
-    console.log("connected")
-    setConnectStatus(true)
+      console.log("connected")
+      setConnectStatus(true);
     }
 
     ws.onclose = () => {
-    console.log("Connection closed")
-    setConnectStatus(false)
-  }
+      console.log("Connection closed")
+      setConnectStatus(false);
+    }
 
-   ws.onmessage = (ev) => {
-    messages.unshift({ name : "bot", message: ev.data})
-    console.log(messages)
-    setMessage(' ')
-  }
+    ws.onmessage = (ev) => {
+      messages.unshift({ name : "bot", message: ev.data})
+      console.log(messages)
+      setMessage(' ')
+    }
 
     ws.onerror = (ev) => {
-    console.log(ev)
+      console.log(ev)
+    }
+
   }
 
-    const onClick = (e) => {
+  const closeConnection = () => {
+    setChatButton(false)
+    ws.close();
+  }
+
+  const onClick = (e) => {
       messages.unshift({ name :"me", message :Message})
       ws.send(Message)
       setMessage('')
     }
 
-     const onChange = (e) => {
+    const onChange = (e) => {
         setMessage(e.target.value);
     }
 
-    return (
-         < ChatLayout onChange={onChange} onClick={onClick} messages = {messages} msg = {Message} connectStatus = {connectStatus}/>
+return (
+    <div>
+      {
+        !chatButton ? (
+    < button className=" animate-bounce fixed p-1 rounded-full bottom-0 right-0 focus:outline-none bg-rose-500 dark:bg-dark-200" onClick={ openConnection }>
+      <RiWechat2Line size={50}/>
+    </button> )  : (
+
+    < button onClick={ closeConnection }>
+    <AiOutlineCloseCircle size={30} className="fixed p-1 z-30 rounded-full bottom-0 right-0 focus:outline-none bg-gray-200 dark:bg-dark-200" />
+    </button> 
     )
+      }
+     { 
+        chatButton ? (
+        < ChatLayout onChange={onChange} onClick={onClick} messages = {messages} msg = {Message} connectStatus = {connectStatus}/>
+        ) : null
+     }
+    </div>
+)
    
     }
