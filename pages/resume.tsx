bbb14ef4199412598af019d024d69a52858f7873
xaviSalazar <xavicoel@gmail.com>
2022-04-15 10:33:24 +0200

    refactor: fix confidential details

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -26,7 +26,7 @@ const resume = () => {
                 <div>
                     <h5 className="my-2 text-xl font-bold">Embedded Systems Intern</h5>
                     <p className="font-semibold"> COCOPARKS (March 2021-September 2021) </p>
-                    <p className="my-3">Responsible for developing an autonomous Embedded System</p>
+                    <p className="my-3">Responsible for developing a low power Embedded System</p>
                 </div>
 
                 <div>
